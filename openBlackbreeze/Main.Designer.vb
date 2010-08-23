23476fd7fb829cee8dc0b722b9ee8374c06c01c7
Fallensn0w <thimcederlund@hotmail.com>
2010-08-23 17:44:58 +0200

    first commit

---
+++ openBlackbreeze/Main.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.lblInfo = New System.Windows.Forms.Label
+        Me.dlgBlackra1n = New System.Windows.Forms.OpenFileDialog
+        Me.dlgSaveBlackra1n = New System.Windows.Forms.SaveFileDialog
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 14)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(427, 64)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Fix my ra1n!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblInfo
+        '
+        Me.lblInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.ForeColor = System.Drawing.Color.White
+        Me.lblInfo.Location = New System.Drawing.Point(-1, 82)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(453, 39)
+        Me.lblInfo.TabIndex = 1
+        Me.lblInfo.Text = "Clicking the button above will solve issues for iTunes 9.1+" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "and making blackra1n" & _
+            " fully working for 3.1.2."
+        Me.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dlgBlackra1n
+        '
+        Me.dlgBlackra1n.Filter = "EXE files|*.exe"
+        Me.dlgBlackra1n.Title = "Select your blackra1n.exe"
+        '
+        'dlgSaveBlackra1n
+        '
+        Me.dlgSaveBlackra1n.FileName = "blackra1n_fixed.exe"
+        Me.dlgSaveBlackra1n.Filter = "EXE Files|*.exe"
+        Me.dlgSaveBlackra1n.Title = "Select where to save your fixed blackra1n.exe"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(451, 122)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Main"
+        Me.ShowIcon = False
+        Me.Text = "openBlackbreeze -- Makes blackra1n breeze.. ;-) By: Fallensn0w"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+    Friend WithEvents dlgBlackra1n As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents dlgSaveBlackra1n As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ openBlackbreeze/Main.vb
@@ -0,0 +1,72 @@
+﻿Public Class Main
+
+    ' A subroutine that will execute a .EXE and wait til its done.
+    Sub DoCMD(ByVal file As String, ByVal arg As String)
+        Dim procNlite As New Process
+        winstyle = 1
+        procNlite.StartInfo.FileName = file
+        procNlite.StartInfo.Arguments = " " & arg
+        procNlite.StartInfo.WindowStyle = winstyle
+        Application.DoEvents()
+        procNlite.Start()
+        Do Until procNlite.HasExited
+            Application.DoEvents()
+            For i = 0 To 5000000
+                Application.DoEvents()
+            Next
+        Loop
+        procNlite.WaitForExit()
+    End Sub
+
+    Function GetDirName(ByVal ScanString As String) As String
+        Dim intPos As Integer = 1, intPosSave As Integer
+        Do
+            intPos = InStr(intPos, ScanString, "\")
+            If intPos = 0 Then
+                Exit Do
+            Else
+                intPos = intPos + 1
+                intPosSave = intPos - 1
+            End If
+        Loop
+        GetDirName = Strings.Left(ScanString, intPosSave)
+    End Function
+
+    Sub bspatch(ByVal oldfile As String, ByVal newfile As String, ByVal patchfile As String)
+        Shell("bspatch.exe " & Chr(34) & oldfile & Chr(34) & " " & Chr(34) & newfile & Chr(34) & " " & Chr(34) & patchfile & Chr(34), AppWinStyle.Hide, True)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+        Button1.Text = "Browsing for blackra1n.exe..."
+        dlgBlackra1n.ShowDialog()
+
+        If dlgBlackra1n.FileName = "" Then End
+        Me.Text = "Select a save location..."
+
+        dlgSaveBlackra1n.ShowDialog()
+        If dlgBlackra1n.FileName = "" Then End
+
+
+        Dim iTunesMobileDevice As String = _
+                        Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) & _
+                            "\Common Files\Apple\Mobile Device Support\iTunesMobileDevice.dll"
+
+        Dim iTunesMobileDevice2 As String = _
+                GetDirName(dlgSaveBlackra1n.FileName) & "iTunesMobileDevice2.dll"
+
+
+        bspatch(dlgBlackra1n.FileName, dlgSaveBlackra1n.FileName, "blackra1n.patch")
+        bspatch(iTunesMobileDevice, iTunesMobileDevice2, "iTunes.patch")
+
+        MessageBox.Show("Your files are now Blackbreezed! The saved blackra1n_fixed.exe will now work for iTunes 9.1+" & vbCrLf & vbCrLf & _
+                                   "Open-sourced version of blackbreeze programmed by Fallensn0w." & vbCrLf & _
+                                   "http://fallensn0w.tk  |  http://fallensn0w.host22.com", _
+        Me.Text, MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1)
+
+        End
+
+    End Sub
+
+
+End Class
